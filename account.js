0d9658026958640fc31c8b7b75294d701503a3e6
Pablo <pablito@me.com>
2023-10-16 11:16:45 +0100

    Added statement function and tests

--- account.js
+++ account.js
@@ -1,4 +1,4 @@
-const Transaction = require('./transaction.js')
+const Transaction = require('./transaction.js');
 
 class Account {
   constructor() {
@@ -6,13 +6,34 @@ class Account {
   }
 
   deposit(amount) {
-    const transaction = new Transaction(amount, 0);
+    const transaction = new Transaction(amount, undefined);
     this.transactions.push(transaction);
   }
 
   withdraw(amount) {
-    const transaction = new Transaction(0, amount);
+    const transaction = new Transaction(undefined, amount);
     this.transactions.push(transaction);
+  }
+
+  statement() {
+    const details = { balance: 0, transactionList: [] };
+    this.transactions.map((transaction) => {
+      this.formatTransactions(details, transaction);
+    });
+    const finalList = details.transactionList.reverse()
+    finalList.unshift('date || credit || debit || balance');
+    return finalList.join('\n');
+  }
+
+  // Helper functions for statement function
+
+  formatTransactions(details, transaction) {
+    details.balance += transaction.credit ? transaction.credit : -transaction.debit;
+    const credit = transaction.credit ? transaction.credit.toFixed(2) : '\b';
+    const debit = transaction.debit ? transaction.debit.toFixed(2) : '\b';
+    details.transactionList.push(
+      `${transaction.date} || ${credit} || ${debit} || ${details.balance.toFixed(2)}`
+    );
   }
 }
 
--- account.test.js
+++ account.test.js
@@ -21,50 +21,79 @@ describe('Account class', () => {
     expect(account.transactions.length).toBe(0);
   });
 
-  test('should add one item to the transactions array when a deposit is made', () => {
-    account.deposit(100);
-    expect(account.transactions.length).toBe(1);
-    expect(Transaction).toHaveBeenCalledWith(100, 0);
-    expect(Transaction).toHaveBeenCalledTimes(1);
-  });
-  
-  test('should add 3 items to the transactions array when 3 deposits are made', () => {
-    account.deposit(1000);
-    account.deposit(10);
-    account.deposit(50);
-    expect(account.transactions.length).toBe(3);
-    expect(Transaction).toHaveBeenCalledWith(1000, 0);
-    expect(Transaction).toHaveBeenCalledWith(10, 0);
-    expect(Transaction).toHaveBeenCalledWith(50, 0);
-    expect(Transaction).toHaveBeenCalledTimes(3);
-  });
+  describe('deposit() and withdraw()', () => {
+    test('should add one item to the transactions array when a deposit is made', () => {
+      account.deposit(100);
+      expect(account.transactions.length).toBe(1);
+      expect(Transaction).toHaveBeenCalledWith(100, undefined);
+      expect(Transaction).toHaveBeenCalledTimes(1);
+    });
+    
+    test('should add 3 items to the transactions array when 3 deposits are made', () => {
+      account.deposit(1000);
+      account.deposit(10);
+      account.deposit(50);
+      expect(account.transactions.length).toBe(3);
+      expect(Transaction).toHaveBeenCalledWith(1000, undefined);
+      expect(Transaction).toHaveBeenCalledWith(10, undefined);
+      expect(Transaction).toHaveBeenCalledWith(50, undefined);
+      expect(Transaction).toHaveBeenCalledTimes(3);
+    });
 
-  test('should add one item to the transactions array when a withdrawal is made', () => {
-    account.withdraw(100);
-    expect(account.transactions.length).toBe(1);
-    expect(Transaction).toHaveBeenCalledWith(0, 100);
-    expect(Transaction).toHaveBeenCalledTimes(1);
-  });
+    test('should add one item to the transactions array when a withdrawal is made', () => {
+      account.withdraw(100);
+      expect(account.transactions.length).toBe(1);
+      expect(Transaction).toHaveBeenCalledWith(undefined, 100);
+      expect(Transaction).toHaveBeenCalledTimes(1);
+    });
+    
+    test('should add 3 items to the transactions array when 3 withdrawals are made', () => {
+      account.withdraw(1000);
+      account.withdraw(10);
+      account.withdraw(50);
+      expect(account.transactions.length).toBe(3);
+      expect(Transaction).toHaveBeenCalledWith(undefined, 1000);
+      expect(Transaction).toHaveBeenCalledWith(undefined, 10);
+      expect(Transaction).toHaveBeenCalledWith(undefined, 50);
+      expect(Transaction).toHaveBeenCalledTimes(3);
+    });
 
-  test('should add 3 items to the transactions array when 3 withdrawals are made', () => {
-    account.withdraw(1000);
-    account.withdraw(10);
-    account.withdraw(50);
-    expect(account.transactions.length).toBe(3);
-    expect(Transaction).toHaveBeenCalledWith(0, 1000);
-    expect(Transaction).toHaveBeenCalledWith(0, 10);
-    expect(Transaction).toHaveBeenCalledWith(0, 50);
-    expect(Transaction).toHaveBeenCalledTimes(3);
-  });
+    test('should add 3 items to the transactions array when 2 withdrawals & a deposit are made', () => {
+      account.withdraw(99);
+      account.deposit(13);
+      account.withdraw(57);
+      expect(account.transactions.length).toBe(3);
+      expect(Transaction).toHaveBeenCalledWith(undefined, 99);
+      expect(Transaction).toHaveBeenCalledWith(13, undefined);
+      expect(Transaction).toHaveBeenCalledWith(undefined, 57);
+      expect(Transaction).toHaveBeenCalledTimes(3);
+    });
+  })
 
-  test('should add 3 items to the transactions array when 2 withdrawals & a deposit are made', () => {
-    account.withdraw(99);
-    account.deposit(13);
-    account.withdraw(57);
-    expect(account.transactions.length).toBe(3);
-    expect(Transaction).toHaveBeenCalledWith(0, 99);
-    expect(Transaction).toHaveBeenCalledWith(13, 0);
-    expect(Transaction).toHaveBeenCalledWith(0, 57);
-    expect(Transaction).toHaveBeenCalledTimes(3);
+  describe('statement()', () => {
+    test('should print a statement with headers only when there are no transactions', () => {
+      expect(account.statement()).toEqual('date || credit || debit || balance')
+    });
+    
+    test('should print a statement with headers and transaction when a £100 deposit has been made', () => {
+      account.deposit(100);
+      const expectedStatement = 'date || credit || debit || balance\n01/04/2000 || 100.00 || || 100.00'
+      expect(account.statement()).toMatch(new RegExp(`^${expectedStatement}$`, 'm'));
+    });
+    
+    test('should print a statement with headers and transaction when a £100 deposit has been made', () => {
+      account.deposit(100);
+      account.withdraw(10);
+      const expectedStatement = 'date || credit || debit || balance\n01/04/2000 || || 10.00 || 90.00\n01/04/2000 || 100.00 || || 100.00'
+      expect(account.statement()).toMatch(new RegExp(`^${expectedStatement}$`, 'm'));
+    });
+    
+    test('should print a statement with headers and correct transaction list in reverse chronological order', () => {
+      account.deposit(1000);
+      account.deposit(2000);
+      account.withdraw(500);
+      const expectedStatement = 'date || credit || debit || balance\n01/04/2000 || || 500.00 || 2500.00\n01/04/2000 || 2000.00 || || 3000.00\n01/04/2000 || 1000.00 || || 1000.00'
+      expect(account.statement()).toMatch(new RegExp(`^${expectedStatement}$`, 'm'));
+    });
   });
 });
